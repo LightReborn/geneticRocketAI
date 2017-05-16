049c92da45ef2ab9e71fabbc84742b81e7f3e838
Michael R. Roark <lightreborn@live.com>
2017-05-16 15:03:46 -0400

    Code works now.
    I forgot to commit a while back and going through projects I noticed the
    issue with this one.

--- src/geneticRocketAI/Rocket.h
+++ src/geneticRocketAI/Rocket.h
@@ -1,18 +1,37 @@
 #pragma once
 
 #include <iterator>
-#include "Genetics.h"
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <vector>
+
+enum GeneMessage {
+	MOVE_FORWARD,
+	ADJUST_ANGLE
+};
+
+struct Gene {
+	GeneMessage commandType;
+	float magnitude;
+};
+
+typedef std::vector<Gene> GeneticCode;
 
 #define ROCKET_SPEED (5)
+#define DNA_LENGTH (100)
+#define MOVE_MAGNITUDE_FLOOR (1.0)
+#define MOVE_MAGNITUDE_CEILING (5.0)
+#define ANGLE_MAGNITUDE_FLOOR (-5.0)
+#define ANGLE_MAGNITUDE_CEILING (5.0)
+//in percentage
+#define RATE_OF_MUTATION (90)
 
 class Rocket {
 
 private:
 	//data members
-	GeneticCode DNA;
+	
 	GeneticCode executedDNA;
 	float xPosition, yPosition;
 	float angle;
@@ -25,20 +44,24 @@ private:
 
 public:
 	//Public Method members
+	GeneticCode DNA;
 	Rocket();
 	Rocket(float x, float y);
-	Rocket(GeneticCode inboundDNA);
-	Rocket(float x, float y, GeneticCode inboundDNA);
+	Rocket(GeneticCode& inboundDNA);
+	Rocket(float x, float y, GeneticCode& inboundDNA);
 	void generateRandomDNA();
 	void copyGenesInto(GeneticCode& outboundDNA);
-	void copyGenesFrom(GeneticCode inboundDna);
-	GeneticCode breedWith(Rocket mateRocket);
+	void copyGenesFrom(GeneticCode& inboundDna);
+	void breedWith(Rocket& mateRocket, GeneticCode& newDNA);
+	void mutate();
 	void executeGene(Gene commandSet);
 	void stepDNA();
 	void executeDNA();
+	void swapDNAWithExecutedDNA();
 	void move(float magnitude);
 	void turn(float degrees);
 	float getX();
 	float getY();
 	bool DNAIsExecuted();
+	void resetPosition(float x, float y);
 };
--- src/geneticRocketAI/driver.cpp
+++ src/geneticRocketAI/driver.cpp
@@ -10,7 +10,18 @@
 //speed in pixels per second
 #define NUMBER_OF_ROCKETS (25)
 
-bool allDNAExecuted(std::vector<Rocket> rockets) {
+#define TARGET_FITNESS (100)
+
+struct Target {
+	float x;
+	float y;
+	Target(int inX, int inY) {
+		x = inX;
+		y = inY;
+	}
+}rocketTarget(WINDOW_HEIGHT/2, 10);
+
+bool allDNAExecuted(std::vector<Rocket>& rockets) {
 
 	for (int i = 0; i < NUMBER_OF_ROCKETS; i++) {
 		if (!rockets[i].DNAIsExecuted()) {
@@ -20,32 +31,104 @@ bool allDNAExecuted(std::vector<Rocket> rockets) {
 	return true;
 }
 
-//todo
-int getHighestFitnessIndex(std::vector<float> fitnessVector) {
+float getDistance(float x1, float y1, float x2, float y2) {
+	float dx = x2 - x1;
+	float dy = y2 - y1;
+
+	return sqrt((dx * dx) + (dy * dy));
+}
+
+int determineFitness(std::vector<Rocket>& rockets, std::vector<float>& fitnessVector) {
+	float fitness = 0.0;
+	for (auto rocket : rockets) {
+		fitness = 1 / getDistance(rocket.getX(), rocket.getY(), rocketTarget.x, rocketTarget.y);
+		if (fitness >= TARGET_FITNESS) {//let us know if we've hit our target.
+			printf("Target of %d fitness reached. Actual: %f", TARGET_FITNESS, fitness);
+			return 1;
+		}
+		fitnessVector.push_back(fitness);
+	}
+
 	return 0;
 }
 
-//todo
-std::vector<float> determineFitness(std::vector<Rocket> rockets) {
-	std::vector<float> fitnessVector;
-	return fitnessVector;
+void getBestRockets(std::vector<Rocket>& rockets, std::vector<float>& fitnessVector, std::vector<Rocket>& newRocketVector) {
+	//get the top performing 20% of rockets.
+	unsigned int howManyRocketsToKeep = NUMBER_OF_ROCKETS / 5;
+
+	while (newRocketVector.size() <= howManyRocketsToKeep - 1) {
+		int indexOfBestRocket = 0;
+		for (unsigned int i = 1; i < rockets.size(); i++) {
+			if (fitnessVector[i] > fitnessVector[indexOfBestRocket]) {
+				indexOfBestRocket = i;
+			}
+		}
+		newRocketVector.push_back(rockets[indexOfBestRocket]);
+		fitnessVector[indexOfBestRocket] = 0.0; //the loop will see the best value as the worst one instead now.
+	}
+}
+
+void generateNewRockets(std::vector<Rocket>& rockets) {
+	//fill the vector with new rockets by using the Rocket.breedWith() method until the vector reach NUMBER_OF_ROCKETS
+	int originalSize = rockets.size() - 1;
+	GeneticCode newDNA;
+
+	//printf("rockets: %d\n", originalSize);
+	for (int i = 1; (i < originalSize) && (rockets.size() < NUMBER_OF_ROCKETS - 1); i++) {
+		rockets[0].breedWith(rockets[i], newDNA);
+	}
+	//printf("out of for loop\n");
+	while (rockets.size() < NUMBER_OF_ROCKETS) {
+		//breed the rest randomly
+		int firstRocketIndex = rand() % rockets.size();
+		int secondRocketIndex = rand() % rockets.size();
+
+		GeneticCode newDNA;
+		rockets[firstRocketIndex].breedWith(rockets[secondRocketIndex], newDNA);
+		rockets.push_back(Rocket(newDNA));
+	}
+	//printf("Finished generating new rockets.\n");
+	
 }
 
 //todo
-std::vector<Rocket> breed(std::vector<Rocket> rockets) {
-	/*	brainstorm:
-	*	pick the top 10-20% most fit objects.
-	*	place all the picked one into a new rocket vector
-	*	The best breeds with all other rockets. (if there is enough size.)
-	*	then randomly breed index 1 through n until the vector fills up (NUMBER_OF_ROCKETS)
-	*/
-	std::vector<float> fitnessVector = determineFitness(rockets);
-	return rockets;
+void mutateAll(std::vector<Rocket>& rockets) {
+	for (auto rocket : rockets) {
+		rocket.mutate();
+	}
+}
+
+//todo
+int breed(std::vector<Rocket>& rockets) {
+	std::vector<float> fitnessVector;
+	std::vector<Rocket> newRocketVector;
+
+	if (determineFitness(rockets, fitnessVector) == 1) {
+		return 1;
+	}
+	getBestRockets(rockets, fitnessVector, newRocketVector);
+
+	rockets.swap(newRocketVector);
+	//free up space.
+	newRocketVector.clear(); 
+	//printf("Length of rockets: %d", rockets.size());
+
+	generateNewRockets(rockets);
+	mutateAll(rockets);
+
+	return 0;
+}
+
+void undoAllExecutions(std::vector<Rocket>& rockets) {
+	for (auto &rocket : rockets) {
+		rocket.swapDNAWithExecutedDNA();
+	}
 }
 
 #undef main
 int main(void) {
 	bool loopFlag = true;
+	bool successfulGeneration = false;
 	float standardX = WINDOW_WIDTH / 2;
 	float standardY = WINDOW_HEIGHT / 2;
 	srand(time(NULL));
@@ -132,6 +215,13 @@ int main(void) {
 		rectVector.push_back(dest);
 	}
 	printf("Number of rects: %d\n", rectVector.size());
+	//The rect representing the target destination.
+	SDL_Rect targetRect;
+	SDL_QueryTexture(tex, NULL, NULL, &targetRect.w, &targetRect.h);
+	targetRect.w = 25;
+	targetRect.h = 25;
+	targetRect.x = rocketTarget.x;
+	targetRect.y = rocketTarget.y;
 
 	printf("Entering display loop\n");
 	//render the rockets at their starting positions.
@@ -140,6 +230,8 @@ int main(void) {
 	for (auto currentRect : rectVector) {
 		SDL_RenderCopy(rend, tex, NULL, &currentRect);
 	}
+	SDL_RenderCopy(rend, tex, NULL, &targetRect);
+
 	while (loopFlag) {
 		SDL_RenderClear(rend);
 
@@ -152,9 +244,9 @@ int main(void) {
 		}
 
 		//printf("Updating rockets.\n");
-		for (int i = 0; i < rocketVector.size(); i++) {
+		for (unsigned int i = 0; i < rocketVector.size(); i++) {
 			rocketVector[i].stepDNA();
-			printf("Main: (%f, %f)\n", rocketVector[i].getX(), rocketVector[i].getY());
+			//printf("Main: (%f, %f)\n", rocketVector[i].getX(), rocketVector[i].getY());
 		}
 
 		//printf("Updating rects.\n");
@@ -168,25 +260,32 @@ int main(void) {
 			SDL_RenderCopy(rend, tex, NULL, &currentRect);
 			//SDL_RenderPresent(rend);
 		}
-		//SDL_RenderCopy(rend, tex, NULL, NULL);
+		SDL_RenderCopy(rend, tex, NULL, &targetRect);
 		SDL_RenderPresent(rend);
-		numbOfLoops++;
+
 
 		if (allDNAExecuted(rocketVector)) {
-			rocketVector = breed(rocketVector);
+			numbOfLoops++;
+			undoAllExecutions(rocketVector);
+			if (breed(rocketVector) == 1){
+				loopFlag = false;
+				successfulGeneration = true;
+			}
+			for (unsigned int i = 0; i < rocketVector.size(); i++) {
+				rocketVector[i].resetPosition(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+			}
 		}
 	}
-
+	printf("\n\n\n");
+	if (successfulGeneration) {
+		printf("Success after %d generations!\n\n\n", numbOfLoops);
+	}
 	//clean up resources
 	SDL_DestroyTexture(tex);
 	SDL_DestroyRenderer(rend);
 	SDL_DestroyWindow(workingWindow);
 	SDL_Quit();
 	
-	for (auto rocket : rocketVector) {
-		printf("(%f, %f)\n", rocket.getX(), rocket.getY());
-	}
-	printf("Loop executions: %d\n", numbOfLoops);
 	system("pause");
 
 	return 0;
